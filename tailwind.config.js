140a7ca9afe7ab44efa435418e56a078b036685c
EJR Bisoj <bishojbk@gmail.com>
2023-09-12 16:50:00 +0545

    Tailwind-Landwind desing

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        purple: "#7E3AF2",
+        grey: "#6B7280",
+        almostwhite: "#F9FAFB",
+      },
+    },
+  },
+  plugins: [],
+};
